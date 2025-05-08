9d3204258552cda583f6b7ba35f0fb6433a5a0d9
abhijeetaretehealthtech <162973379+abhijeetaretehealthtech@users.noreply.github.com>
2025-05-08 13:09:05 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+        <div className='text-black'>
+          Travel app
+        </div>
+    </>
+  )
+}
+
+export default App
